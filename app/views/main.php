3e8aa0f29044b2feccaf1ddaa0b5f8692482d9d9
ahmadbiz <ahmad.baiazid@gmail.com>
2017-10-20 22:05:00 -0400

    fixing branch

--- app/views/main.php
+++ app/views/main.php
@@ -1,8 +1,14 @@
 <html>
 <head>
+<<<<<<< HEAD
+	<title>CompStore343</title>
+	<link rel="stylesheet" href="<?php echo ROOT_PATH; ?>/content/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo ROOT_PATH; ?>/content/css/light-bootstrap-dashboard.css">
+=======
 	<title>Shareboard</title>
 	<link rel="stylesheet" href="<?php echo ROOT_PATH; ?>assets/css/bootstrap.css">
 	<link rel="stylesheet" href="<?php echo ROOT_PATH; ?>assets/css/style.css">
+>>>>>>> f1a9167787a819c5a0b020e52c82c445c75f18aa
 </head>
 <body>
 	<nav class="navbar navbar-default">
@@ -17,12 +23,25 @@
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
+<<<<<<< HEAD
+              <li><a href="<?php echo ROOT_URL; ?>">Home</a></li>
+              <li><a href="<?php echo ROOT_URL;?>admin/adminLogin">Login as Administrator</a></li>
+          </ul>
+            <ul class="nav navbar-nav navbar-right">
+            <?php if(isset($_SESSION['is_logged_in'])) : ?>
+            <li><a href="<?php echo ROOT_URL; ?>">Welcome <?php echo $_SESSION['user_data']['FirstName']; ?></a></li>
+=======
             <li><a href="<?php echo ROOT_URL; ?>">Home</a></li>
           </ul>
 
           <ul class="nav navbar-nav navbar-right">
             <?php if(isset($_SESSION['is_logged_in'])) : ?>
+<<<<<<< HEAD
             <li><a href="<?php echo ROOT_URL; ?>">Welcome <?php echo $_SESSION['user_data']['name']; ?></a></li>
+>>>>>>> f1a9167787a819c5a0b020e52c82c445c75f18aa
+=======
+            <li><a href="<?php echo ROOT_URL; ?>">Welcome <?php echo $_SESSION['user_data']['FirstName']; ?></a></li>
+>>>>>>> f5a43f70e94070f97c4cd2830ddd01d7fcb309fc
             <li><a href="<?php echo ROOT_URL; ?>users/logout">Logout</a></li>
           <?php else : ?>
             <li><a href="<?php echo ROOT_URL; ?>users/login">Login</a></li>
--- app/views/users/login.php
+++ app/views/users/login.php
@@ -1,10 +1,23 @@
+<<<<<<< HEAD
+
+=======
+>>>>>>> f1a9167787a819c5a0b020e52c82c445c75f18aa
 <div class="panel panel-default">
   <div class="panel-heading">
     <h3 class="panel-title">User Login</h3>
   </div>
   <div class="panel-body">
+<<<<<<< HEAD
 
+<<<<<<< HEAD
+=======
+    <!--Uncomment the following line to try out an example  -->
+    <!--<?= $apple . '<br>' .$samsung ?> -->
+>>>>>>> f5a43f70e94070f97c4cd2830ddd01d7fcb309fc
+    <form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>">
+=======
     <form method="POST" action="<?php //$_SERVER['PHP_SELF']; ?>../controllers/loginController.php">
+>>>>>>> f1a9167787a819c5a0b020e52c82c445c75f18aa
       	<div class="form-group">
     		<label>Email</label>
     		<input type="text" name="email" class="form-control" />
