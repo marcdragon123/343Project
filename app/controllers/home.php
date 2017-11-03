d5517acce4b97596243b66d54e2a5510467999d1
Anania-y <anania.y@hotmail.com>
2017-11-03 16:57:08 -0400

    working require class
    let me know if it works

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -5,16 +5,17 @@
  * Date: 2017-11-02
  * Time: 9:38 PM
  */
-require 'Controller.php';
+require '../Classes/Controller.php';
 
 class home extends Controller
 {
     //$var is a variable passed in the Url
     // /controller/function/var1/var2
     public function index($var='', $var2=''){
+    	
     $home = $this->model('home');
     }
-
+    
     public function test(){
 
     }
