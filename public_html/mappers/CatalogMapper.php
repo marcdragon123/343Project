faedc7450317db7a27f539ebd6d285100e81e24b
leban <leban@DESKTOP-NP56T40>
2017-11-19 17:30:59 -0500

    Catalog controller linked with Mapper, TDG and DB

---
+++ public_html/mappers/CatalogMapper.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leban
+ * Date: 2017-11-19
+ * Time: 1:44 PM
+ */
+
+class CatalogMapper
+{
+
+}
---
+++ public_html/views/user/browseCatalog.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leban
+ * Date: 2017-11-19
+ * Time: 1:34 PM
+ */
