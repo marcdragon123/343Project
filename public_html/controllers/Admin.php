8ea68d44326179e5020c3455445471880c21d896
Anania-y <anania.y@hotmail.com>
2017-11-20 00:24:02 -0500

    Admin login

--- public_html/controllers/Admin.php
+++ public_html/controllers/Admin.php
@@ -1,17 +1,44 @@
 <?php
 /**
- * Created by PhpStorm.
- * users: ahmadbiz
- * Date: 2017-11-05
- * Time: 3:25 PM
+ * Anania
  */
+class admin extends Controller {
 
-class Admin extends Controller {
-    protected $userMapper;
+    private $obj;
 
-    public function login(){
-        $this->userMapper = new CustomerMapper();
+    public function __construct($action, $request) {
+        parent::__construct($action, $request);
+    }
 
+    /**
+     * this function will instantiate a users object and also push it to the db to get an id for the users
+     */
+    public function register() {
+        $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        if($post['submit']) {
+            $customer = CustomerMapper::getInstance()->create($post);
+
+
+
+            if(!is_null($customer)) {
+
+                header('Location: ' . ROOT_URL . 'admin/adminlogin');
+            }
+        }
+        $this->returnView(null, true);
+    }
+
+    /**
+     * this function will call
+     */
+    public function adminlogin(){
+        $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        if($post['submit']){
+            if(CustomerMapper::getInstance()->login($post)){
+                header('Location: ' . ROOT_URL . 'catalog');
+            }
+        }
+        $this->returnView(null, true);
     }
 
     public function logout(){
@@ -22,7 +49,15 @@ class Admin extends Controller {
 
     }
 
-    public function viewProductSpecification($id){
+    public function viewProductSpecification(){
+
+    }
+
+    public function browseCatalog(){
+        $viewmodel = CatalogMapper::getInstance();
+
+        $this->returnView($viewmodel->selectAll(),true);
+
 
     }
 
--- public_html/views/Admin/adminlogin.php
+++ public_html/views/Admin/adminlogin.php
@@ -1,12 +1,16 @@
-<?php
-/**
- * Created by PhpStorm.
- * users: leban
- * Date: 2017-11-18
- * Time: 6:41 PM
- */
-
-class adminlogin
-{
-
-}
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h3 class="panel-title">Admin Login</h3>
+    </div>
+    <div class="panel-body">
+        <form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>">
+            <div class="form-group">
+                <input type="text" name="Email" placeholder="Email" class="form-control" />
+            </div>
+            <div class="form-group">
+                <input type="password" name="Password" placeholder="Password" class="form-control" />
+            </div>
+            <input class="btn btn-primary" name="submit" type="submit" value="Submit"/>
+        </form>
+    </div>
+</div>
