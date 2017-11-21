8035422aa65a537d5ba6a1b05a2415f4b48a6f13
Anania-y <anania.y@hotmail.com>
2017-11-20 22:26:21 -0500

    hard reset

--- app/views/catalog/DisplayCatalogue.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: leban
- * Date: 2017-10-27
- * Time: 5:28 PM
- */
--- app/views/home/index.php
+++
@@ -1,6 +0,0 @@
-
-<div class="text-center">
-	<h1>Welcome To 343Compstore</h1>
-	<p class="lead">Login or Click Below to browse our Catalog!</p>
-	<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH;?>views/users/browseCatalog">Browse Catalog</a>
-</div>
