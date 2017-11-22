481813fb8b9ad2a6afeb80db319203ebc26a95df
Roy Saliba <rsaliba95@icloud.com>
2017-11-21 23:23:51 -0500

    Update ProductCatalog.php

--- domain/core/ProductCatalog.php
+++ domain/core/ProductCatalog.php
@@ -1,61 +1,106 @@
 <?php
-/**
- * Created by PhpStorm.
- * users: ahmadbiz
- * Date: 2017-11-05
- * Time: 3:33 PM
- */
 
-class ProductCatalog{
+class ProductCatalog
+{
 
 
-    protected $productMapper;
     protected $productContainer = array();
     protected $productsInUse = array();
+    private $containerFile;
+    public $myMapper;
 
     private static $instance;
 
-    public static function getInstance(){
-        if(self::$instance === null){
+    /**
+     * @return ProductCatalog
+     */
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
             self::$instance = new ProductCatalog();
         }
-
         return self::$instance;
     }
 
     private function __construct()
     {
-
+        $this->myMapper = CatalogMapper::getInstance();
+        $this->containerFile = new File('productContainer.txt');
     }
 
+    /**
+     * @param Product $product
+     * @throws Exception
+     */
+    public function addProduct(Product $product)
+    {
+        $this->productContainer = $this->getProductContainer();
 
-    public function addProduct(Product $product){
-
-        if(isset($this->productContainer[$product->__get('ProductType')][$product->__get('SerialNumber')])){
+        if (isset($this->productContainer[$product->__get('ProductType')][$product->__get('SerialNumber')])) {
             throw new Exception("Product Serial Number Already Exists");
         }
         $this->productContainer[$product->__get('ProductType')][$product->__get('SerialNumber')] = $product;
+        $this->containerFile->write($this->productContainer, true);
     }
 
-    public function editProduct(Product $product){
-        if(!isset($this->productContainer[$product->__get('ProductType')][$product->__get('SerialNumber')])){
+    /**
+     * @param Product $product
+     * @throws Exception
+     */
+    public function editProduct(Product $product)
+    {
+        $this->productContainer = $this->getProductContainer();
+        if (!isset($this->productContainer[$product->__get('ProductType')][$product->__get('SerialNumber')])) {
             throw new Exception("Product is not in the Product Catalog");
         }
         $this->productContainer[$product->__get('ProductType')][$product->__get('SerialNumber')] = $product;
+        $this->containerFile->write($this->productContainer, true);
 
     }
 
-    public function deleteProductSpecification($productType, $productSerialNumber){
-
+    /**
+     * @param Product $product
+     * @throws Exception
+     */
+    public function deleteProduct(Product $product)
+    {
+        $this->productContainer = $this->getProductContainer();
+        if (!isset($this->productContainer[$product->__get('ProductType')][$product->__get('SerialNumber')])) {
+            throw new Exception("Product is not in the Product Catalog");
+        }
+        unset($this->productContainer[$product->__get('ProductType')][$product->__get('SerialNumber')]);
     }
 
-    public function viewByType($productType){
+    /**
+     * @param $productType
+     */
+    public function viewByType($productType)
+    {
+        $this->productContainer = $this->getProductContainer();
 
+        //foreach ($this->productContainer[$productType] as $products){
+        //$this->myMapper
+        //}
     }
 
-    public function viewAllProducts(){
+    /**
+     *
+     */
+    public function viewAllProducts()
+    {
+        $this->productContainer = $this->getProductContainer();
 
+        //foreach ($this->productContainer as $type => $spec){
+        //$this->
+        //}
     }
 
-
+    /**
+     * @return mixed
+     */
+    public function getProductContainer()
+    {
+        $temp = $this->containerFile->read($this->containerFile->getFileName());
+        return $temp[0];
+    }
 }
