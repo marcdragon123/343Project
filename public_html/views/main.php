e7a94924475ec08ccedbc120003141e245c37dc6
Vartan Benohanian <vartanbeno@gmail.com>
2017-12-01 03:05:06 -0500

    cleaned up navbar

--- public_html/views/main.php
+++ public_html/views/main.php
@@ -17,18 +17,16 @@
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <?php if (isset($_SESSION['is_logged_in'])) :?>
-                    <?php if($_SESSION['user_data']['Type'] === 'A') : ?>
+                <?php if (isset($_SESSION['is_logged_in']) && $_SESSION['user_data']['Type'] === 'A') :?>
                         <li><a href="<?php echo ROOT_URL; ?>admin">Home</a></li>
                         <li><a href = "<?php echo ROOT_URL; ?>admin/viewProductCatalog">View Catalog</a></li>
-                    <?php else :?>
-                        <li><a href="<?php echo ROOT_URL; ?>">Home</a></li>
-                    <?php endif;?>
+                <?php else :?>
+                <li><a href="<?php echo ROOT_URL; ?>">Home</a></li>
                 <?php endif;?>
 
                 <?php if(isset($_SESSION['is_logged_in'])) : ?>
                 <?php else :?>
-                    <li><a href="<?php echo ROOT_URL; ?>admin/adminlogin">Administrator</a></li>
+                    <li><a href="<?php echo ROOT_URL; ?>admin/adminlogin">Admin Login</a></li>
                 <?php endif; ?>
 
 
